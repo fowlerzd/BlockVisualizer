9b09a730cdac16bae7f7a60e59c5340f08a0caba
fowlerzd <155563410+fowlerzd@users.noreply.github.com>
2024-05-23 14:36:00 -0400

    Add files via upload

--- Blocks.py
+++ Blocks.py
@@ -1,27 +1,47 @@
+# import libraries
 from turtle import Turtle
 import math
 from numpy import matmul
+
+# read othographic points
+file = open('othographics.txt', 'r')
+
+# initialize point lists
+top_points = []
+front_points = []
+side_points = []
+
+# first line
+line = file.readline()
+
+# read file
+if "Top" in line:
+    # read in top points
+    line = file.readline()
+    while "Front" not in line:
+        coords = [int(x.strip()) for x in line.split(',')]
+        top_points.append(coords)
+        line = file.readline()
+    # read in front points
+    line = file.readline()
+    while "Side" not in line:
+        coords = [int(x.strip()) for x in line.split(',')]
+        front_points.append(coords)
+        line = file.readline()
+    # read in side points
+    line = file.readline()
+    while line != "":
+        coords = [int(x.strip()) for x in line.split(',')]
+        side_points.append(coords)
+        line = file.readline()
+
+# debug
+print(top_points, front_points, side_points)
 
 # initialize the screen
 writer = Turtle()
 writer.screen.screensize(500, 500)
 writer.screen.title("Block Visualizer")
-
-# 3d points on a cube
-point1_3d = [0, 0, 0]
-point2_3d = [0, 0, 50]
-point3_3d = [0, 50, 0]
-point4_3d = [0, 50, 50]
-point5_3d = [50, 0, 0]
-point6_3d = [50, 0, 50]
-point7_3d = [50, 50, 0]
-point8_3d = [50, 50, 50]
-
-# list of points to be drawn
-points_3d = [point1_3d, point3_3d, point4_3d, point2_3d, point1_3d]
-
-# initilize the list of 2d points
-points_2d = points_3d
 
 # this function will return a isomorphized 2d point from a 3d point   
 def transform_dimentions(point_3d):
@@ -35,6 +55,7 @@ def transform_dimentions(point_3d):
     x_cord = 0
     y_cord = 0
 
+    # matrix multi
     for i in range(len(point_3d)):
         x_cord += transformation_matrix[0][i]*point_3d[i]
         y_cord += transformation_matrix[1][i]*point_3d[i]
@@ -43,12 +64,11 @@ def transform_dimentions(point_3d):
 
     return point_2d
 
-# transform 3d points into 2d
-for i in range(len(points_3d)):
-    points_2d[i] = transform_dimentions(points_3d[i])
 
-# draw each desired point (side of a cube)
-for point in points_2d:
-    writer.goto(point)
+
+# this function draws all points in a list
+def draw_points(points):
+    for point in points:
+        writer.goto(point)
 
 writer.screen.mainloop()
