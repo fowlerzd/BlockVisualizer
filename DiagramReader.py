0f49b55520f733b8f5e9d794b85941cbfeda23c2
fowlerzd <155563410+fowlerzd@users.noreply.github.com>
2024-06-16 20:02:55 -0400

    Update DiagramReader.py
    Reading 2 pixels

--- DiagramReader.py
+++ DiagramReader.py
@@ -1,15 +1,13 @@
 import cv2
  
-# save read image into different file formats
+# import bmp image
 bmp = cv2.imread('test1.bmp') 
-jpg = cv2.imread('test1.jpg') 
-png = cv2.imread('test1.png') 
-tga = cv2.imread('test1.tga') 
-tiff = cv2.imread('test1.tiff')
 
-# print what a pixel of each format looks like
-print('bmp: ', bmp[0], '\n')
-print('jpg: ', jpg[0], '\n')
-print('png: ', png[0], '\n')
-#print('tga: ', tga[0], '\n')
-print('tiff: ', tiff[0], '\n')
+
+print("Height: ", bmp.shape[0])
+print("Width: ", bmp.shape[1])
+print("RGB Values: ", bmp.shape[2])
+
+# print pixel 0, 0 (height, width)
+print(bmp.view()[0][0]) # [0 0 0] is black
+print(bmp.view()[0][6]) # [255 255 255] is white
