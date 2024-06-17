e753537d1696579f598a8938d4b8aa522525f14d
fowlerzd <155563410+fowlerzd@users.noreply.github.com>
2024-06-16 20:55:34 -0400

    Update DiagramReader.py

--- DiagramReader.py
+++ DiagramReader.py
@@ -1,13 +1,46 @@
 import cv2
  
 # import bmp image
-bmp = cv2.imread('test1.bmp') 
+img = cv2.imread('test1.bmp') 
+
+# read in image dimesions
+IMG_HEIGHT = img.shape[0]
+IMG_WIDTH = img.shape[1]
+IMG_DEPTH = img.shape[2]
+
+print("Image Height: ", IMG_HEIGHT)
+print("Image Width: ", IMG_WIDTH)
+print("Image Depth: ", IMG_DEPTH)
 
 
-print("Height: ", bmp.shape[0])
-print("Width: ", bmp.shape[1])
-print("RGB Values: ", bmp.shape[2])
+# find horizontal seperation
+white_count = 0
+widest_white_count_row = 0
+# loop through image
+for row in range(IMG_HEIGHT):
+    white_count = 0
+    for col in range(IMG_WIDTH):
+        if((img.view()[row][col] == [255, 255, 255]).all()): # check if pixel is white
+            white_count += 1
+            if(white_count == IMG_WIDTH): # row is found
+                widest_white_count_row = row
+                break
+        else:
+            white_count = 0
+print("Widest White Row: ", widest_white_count_row)
 
-# print pixel 0, 0 (height, width)
-print(bmp.view()[0][0]) # [0 0 0] is black
-print(bmp.view()[0][6]) # [255 255 255] is white
+# find vertical seperation
+white_count = 0
+tallest_white_count_col = 0
+# loop through image
+for col in range(IMG_WIDTH):
+    white_count = 0
+    for row in range(IMG_HEIGHT):
+        if((img.view()[row][col] == [255, 255, 255]).all()): # check if pixel is white
+            white_count += 1
+            if(white_count == IMG_HEIGHT): # tallest collumn is found
+                tallest_white_count_col = col
+                break
+        else:
+            white_count = 0
+print("Tallest White Col: ", tallest_white_count_col)
